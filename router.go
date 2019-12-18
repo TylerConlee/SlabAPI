d3a0561db4288e3222dbd999ff2685a838c77305
Tyler Conlee <tylerlconlee@gmail.com>
2019-12-18 14:03:53 -0800

    move to table based testing for multiple GET routes

--- router.go
+++ router.go
@@ -14,6 +14,7 @@ import (
 func NewRouter() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", IndexRouter)
+	r.HandleFunc("/config", GetConfigHandler)
 	http.ListenAndServe(":8000", nrgorilla.InstrumentRoutes(r, nrApp))
 }
 
@@ -26,4 +27,15 @@ func IndexRouter(w http.ResponseWriter, r *http.Request) {
 
 	// TODO: add in additional health metrics, such as uptime, database connections, etc.
 	io.WriteString(w, `{"alive": true}`)
+}
+
+// IndexRouter is the root level endpoint that's returned when a user requests the "/" endpoint.
+// This route will likely be utilized in the future to show the current server status.
+func GetConfigHandler(w http.ResponseWriter, r *http.Request) {
+	// Using JSON, set a very basic health check
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// TODO: add in additional health metrics, such as uptime, database connections, etc.
+	io.WriteString(w, `{"alive": false}`)
 }
--- router_test.go
+++ router_test.go
@@ -6,32 +6,46 @@ import (
 	"testing"
 )
 
-func TestIndexHandler(t *testing.T) {
-	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
-	// pass 'nil' as the third parameter.
-	req, err := http.NewRequest("GET", "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+var gettests = []struct {
+	name     string
+	route    string
+	handler  http.HandlerFunc
+	expected string
+}{
+	{"GET - IndexHandler", "/", IndexRouter, `{"alive": true}`},
+	{"GET - ConfigHandler", "/config", GetConfigHandler, `{"alive": false}`},
+}
 
-	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(IndexRouter)
+func TestGETHandlers(t *testing.T) {
+	for _, tt := range gettests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+			// pass 'nil' as the third parameter.
+			req, err := http.NewRequest("GET", tt.route, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
-	// directly and pass in our Request and ResponseRecorder.
-	handler.ServeHTTP(rr, req)
+			// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(tt.handler)
 
-	// Check the status code is what we expect.
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
+			// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+			// directly and pass in our Request and ResponseRecorder.
+			handler.ServeHTTP(rr, req)
 
-	// Check the response body is what we expect.
-	expected := `{"alive": true}`
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+			// Check the status code is what we expect.
+			if status := rr.Code; status != http.StatusOK {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, http.StatusOK)
+			}
+
+			// Check the response body is what we expect.
+
+			if rr.Body.String() != tt.expected {
+				t.Errorf("handler returned unexpected body: got %v want %v",
+					rr.Body.String(), tt.expected)
+			}
+		})
 	}
 }
