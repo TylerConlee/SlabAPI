c44fcb24a5ebc4b588d47ebb1d7814d502bffcce
Tyler Conlee <tylerlconlee@gmail.com>
2020-01-30 14:05:18 -0800

    add update function for zendesk config

--- resolver/config_resolver.go
+++ resolver/config_resolver.go
@@ -11,7 +11,6 @@ import (
 // Zendeskconfig retrieves the Zendesk configuration from the postgres database
 // and returns it as a Zendeskconfig model.
 func (r *queryResolver) Zendeskconfig(ctx context.Context) (*model.ZendeskConfig, error) {
-
 	con, err := db.Query(`SELECT zen_user, zen_apikey, zen_url FROM config`)
 	if err != nil {
 		log.Fatal(err.Error())
@@ -38,7 +37,17 @@ func (r *queryResolver) Postgresconfig(ctx context.Context) (*model.PostgresConf
 // ***** UPDATE (PUT) config functions ***** //
 
 func (r *mutationResolver) UpdateZendeskConfig(ctx context.Context, user string, apikey string, url string) (*model.ZendeskConfig, error) {
-	panic("not implemented")
+	var result model.ZendeskConfig
+
+	conf, err := db.Prepare("UPDATE config SET zen_user = $1, zen_apikey = $2, zen_url = $3")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	conf.Exec(user, apikey, url)
+	result = model.ZendeskConfig{User: user, Apikey: apikey, URL: url}
+	defer db.Close()
+	return &result, err
+
 }
 func (r *mutationResolver) UpdateSlackConfig(ctx context.Context, apikey string, channel string) (*model.SlackConfig, error) {
 	panic("not implemented")
