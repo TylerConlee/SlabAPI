f81dc6f32ea11473fd724544aa76e7c60c403cf6
Tyler Conlee <tylerlconlee@gmail.com>
2020-04-24 14:05:57 -0700

    update table creation

--- datastore/tables.go
+++ datastore/tables.go
@@ -7,7 +7,7 @@ import "log"
 func createTables(db *sql.DB) {
 	tables := make([]string, 0)
 	// Create configuration table
-	tables = append(tables, `create table if not exists config (zen_user text not null, zen_apikey text not null, zen_url text not null, slack_apikey text not null, slack_channel text not null);`)
+	tables = append(tables, `create table if not exists config (zen_user text not null, zen_apikey text not null, zen_url text not null);`)
 	for _, table := range tables {
 		if _, err := db.Exec(table); err != nil {
 			return
