e5f86d9fd69829cc3be9c27f348f8ea6fd61b0a1
Tyler Conlee <tylerlconlee@gmail.com>
2021-04-14 09:16:15 -0700

    add comment

--- zendesk/users.go
+++ zendesk/users.go
@@ -6,7 +6,11 @@ import (
 	"github.com/tylerconlee/SlabAPI/model"
 )
 
-func(c *Client) GetUser(ctx.Context) (output *model.User, err Error) {
+// GetUser takes the client, c, and requests the details for the
+// user provided by the context to the Zendesk API wrapper. Once it
+// retreives that data from Zendesk, it converts the output into a model.
+// user.
+func (c *Client) GetUser(ctx.Context) (output *model.User, err Error) {
 	o, err := c.client.GetUser(ctx, int64(id))
 	output = &model.User{
 		Active: o.Active,
